e0e74cb65f013eb1ec8ff69093dc499ebc84805d
ATufekovic <anto.tufekovic@gmail.com>
2020-06-27 18:49:06 +0200

    content defined, starting work on text based task sharing

--- web/content.php
+++ web/content.php
@@ -3,6 +3,7 @@ session_start();
 if (!isset($_SESSION["username"])) {
     header("Location: index.php");
 }
+aaaaaaaaaaaaaaaaa
 ?>
 
 <!DOCTYPE html>
@@ -22,13 +23,29 @@ if (!isset($_SESSION["username"])) {
     <div class="jumbotron text-center">
         <h2>Title</h2>
     </div>
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
-            <div class="col"></div>
-            <div class="col">
+            <div class="col-sm-3">
                 <div class="card">
                     <div class="card-header">
-                        <h4>Hello</h4>
+                        <h4>General task view</h4>
+                    </div>
+                    <div class="card-body">
+                        <p>Click on a task to see more information about it.</p>
+                        <p>Tasks can be public and private.</p>
+                        <p>If you own a task you can edit it.</p>
+                        <p>If a task is public others can see it but they can't edit it.</p>
+                    </div>
+                    <div class="card-footer">
+                        <p><a href="newTask.php" class="btn btn-outline-dark">Click here</a> to create a new task</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6"></div>
+            <div class="col-sm-3">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Logged in as <?php echo $_SESSION["username"]; ?></h4>
                     </div>
                     <div class="card-body">
                         <p>Some irrelevant info, TODO: change this</p>
@@ -38,7 +55,6 @@ if (!isset($_SESSION["username"])) {
                     </div>
                 </div>
             </div>
-            <div class="col"></div>
         </div>
     </div>
 </body>
